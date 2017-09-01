ff7963331216ad0ad99553a56857ea8656a7223c
juanfresia <juanmanuelfresia@gmail.com>
2017-09-01 17:54:45 -0300

    Example API: use POST to add users
    Signed-off-by: juanfresia <juanmanuelfresia@gmail.com>

--- src/main/myApp.py
+++ src/main/myApp.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
-from resources.SimpleAPI import Hello, GoodBye
+from resources.SimpleAPI import Hello, GoodBye, Greet, GreetAdd
 
 application = Flask(__name__)
 api = Api(application)
 
 api.add_resource(Hello, '/')
 api.add_resource(GoodBye, '/goodbye')
-
+api.add_resource(Greet, '/greet/<int:id>')
+api.add_resource(GreetAdd, '/greet')
 
 if __name__ == "__main__":
     application.run(host='localhost')
--- src/main/resources/SimpleAPI.py
+++ src/main/resources/SimpleAPI.py
@@ -1,11 +1,10 @@
 from flask_restful import Resource
+from flask import jsonify, abort, request
 
 class Hello(Resource):
     def get(self):
         print("GET at /")
         return 'Hello' 
-
-
 
 class GoodBye(Resource):
     def get(self):
@@ -17,3 +16,45 @@ class GoodBye(Resource):
         return {'good':'bye'}
 
 
+
+users = [
+        { 'id': 1, 'name': "Juan"},
+        { 'id': 2, 'name': "Ale"},
+        { 'id': 3, 'name': "Cami"},
+        { 'id': 6, 'name': "Euge"}
+    ]
+
+class Greet(Resource):
+    def get(self, id):
+        print("GET at /greet/id")
+        candidates = [user for user in users if user['id'] == id]
+        if len(candidates) == 0:
+            abort(404)
+        return jsonify({'greetings': candidates[0]})
+
+
+
+class GreetAdd(Resource):
+    def get(self):
+        print("GET at /greet")
+        return jsonify({'users' : users})
+
+    def post(self):
+        print(request.json)
+        if (not request.json or not 'user' in request.json):
+            abort(400)
+        if (not 'id' in  request.json['user'] or not 'name' in request.json['user']):
+            abort(400)
+
+        id = request.json['user']['id']
+
+        for user in users:
+            iaf user['id'] == id:
+                abort(400)
+
+        name = request.json['user']['name']
+        newUser = { 'id' : id, 'name' : name }
+
+        users.append(newUser)
+        print("POST at /greet")
+        return jsonify({'user' : newUser})
