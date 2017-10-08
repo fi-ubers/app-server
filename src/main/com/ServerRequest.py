9a1fe31d14e79b2f973592e1f335dc3ae9d52985
emariotti3 <eugenia.mariotti@live.com.ar>
2017-10-08 19:02:05 -0300

    Integrating PUT/DELETE/GET by id operations to User API.

--- src/main/com/ServerRequest.py
+++ src/main/com/ServerRequest.py
@@ -30,7 +30,6 @@ MAX_ATTEMPTS = 10
 #		return None 
 
 """Returns a list of all the users and their information in json format."""
-#TESTED
 def getUsers():
 	r = requests.get(USER_END)
 	if (r.status_code != constants.SUCCESS):
@@ -41,7 +40,6 @@ def getUsers():
 
 """Receives a user id. Returns the information of the user that matches that id in json format.
 """
-#TESTED
 def getUser(userId):
 	r = requests.get(USER_END + "/" + str(userId))
 	if (r.status_code != constants.SUCCESS):
@@ -50,26 +48,21 @@ def getUser(userId):
 	return r.json()["user"]
 
 
-
 """Attempts to perform a request. If the request is rejected because of a reference missmatch, the 
 operation will be repeated until successfully completed or until a maximum number of attempts is 
 reached or until another error arises."""
-#TESTED
 def _permformUpdate(f, endpoint, updatedEntityName, updatedEntity):
 	r = f(endpoint, updatedEntity)
 	attempts = 0
-	try:
-		while (r.status_code == constants.UPDATE_CONFLICT) and (attempts < MAX_ATTEMPTS):
-			print ("UPDATE FAILED, RETRYING...")
-			newData = json.loads(requests.get(endpoint).text)
-			updatedEntity["_ref"] = newData[updatedEntityName]["_ref"]
-			r = f(endpoint, updatedEntity)
-			attempts += 1
-		if (r.status_code == constants.UPDATE_CONFLICT):
-			logger.getLogger().error("Attempted to update user. Request failed with reference error.")
-			raise ValueError("Error attempting to update entity. Try again later.")
-	except requests.exceptions.Timeout:
-		logger.getLogger().error("Attempted to update user. Request timed-out")	
+	while (r.status_code == constants.UPDATE_CONFLICT) and (attempts < MAX_ATTEMPTS):
+		print ("UPDATE FAILED, RETRYING...")
+		newData = json.loads(requests.get(endpoint).text)
+		updatedEntity["_ref"] = newData[updatedEntityName]["_ref"]
+		r = f(endpoint, updatedEntity)
+		attempts += 1
+	if (r.status_code == constants.UPDATE_CONFLICT):
+		logger.getLogger().error("Attempted to update user. Request failed with reference error.")
+		raise ValueError("Error attempting to update entity. Try again later.")
 	return r
 
 """Receives a user represented by a json structure and attempts to update its data.
@@ -85,7 +78,6 @@ def updateUser(user_js):
 	return (True, r.json()['user'])
 
 """ Asks shared server to create a new user"""
-#TESTED
 def createUser(user_js):
 	user_js["_ref"] = ""
 	r = requests.post(os.environ["SS_URL"] + USER_END, data = json.dumps(user_js), headers=headers)
@@ -97,7 +89,6 @@ def createUser(user_js):
 """Receives a user represented by a json structure and validates its credentials.
 Returns True if the credentials were invalid, returns False otherwise.
 """
-#TESTED
 def validateUser(user_js):
 	userInfo = {"username" : user_js['username'], "password" : user_js['password'], "facebookAuthToken" : user_js['fbToken'] }
 
@@ -115,14 +106,13 @@ def validateUser(user_js):
 """Receives a user id and attempts to delete it. Returns True if the user exists and is correctly deleted.
 Returns False if the user id does not match any user id.
 """
-#TESTED
 def deleteUser(userId):
 	r = requests.delete(USER_END + "/" + str(userId))
 	if (r.status_code == constants.NOT_FOUND):
-		return False
+		return (False, r.status_code)
 	if (r.status_code != constants.DELETE_SUCCESS):
 		logger.getLogger("Shared Server returned error: %d"%(r.status_code))
 		raise Exception("Shared Server returned error: %d"%(r.status_code))
-	return True
+	return (True, r.status_code)
 
 
